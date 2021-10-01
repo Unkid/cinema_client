fa9b171d1a9672b3aca0a802440968811b8eea26
Alex Unkid <sashabelova99@gmail.com>
2021-10-02 00:07:59 +0300

    Create db model

--- cinema_server/index.js
+++ cinema_server/index.js
@@ -1,30 +1,35 @@
+require('dotenv').config()
 const express = require('express');
-
-
+const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require('cors')
 
 const app = express();
 
 app.set('port', process.env.PORT || 5000);
 
-app.get('/', (req,res) =>{
-    res.type('text/plain');
-    res.send('Афиша кинотеатров')
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req,res) => {
+    res.status(200).json({message: 'WORKING!'})
 })
 
-app.use((req,res)=> {
-    res.type('text/plain')
-    res.status(404);
-    res.send('404 - не найдено')
-});
+const start = async() => {
+    try{
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(app.get('port'), ()=> {
+            console.log('Express запущен на localhost:'+
+            app.get('port')+ '; Нажмите Ctrl+C для завершения')
+        })
+    }
+    catch(e){
+        console.log(e)
 
-app.use((err, req, res, next)=> {
-    console.log(err.stack)
-    res.type('text/plain');
-    res.status(500);
-    res.send('500 - ошибка сервера')
-});
+    }
+}
 
-app.listen(app.get('port'), ()=> {
-    console.log('Express запущен на localhost:'+
-    app.get('port')+ '; Нажмите Ctrl+C для завершения')
-})
+start()
+
+
---
+++ cinema_server/models/models.js
@@ -0,0 +1,127 @@
+const sequelize = require('../db')
+const {DataTypes} = require('sequelize')
+
+const Movie = sequelize.define('movie', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    title: {type: DataTypes.STRING, allowNull: false},
+    country: {type: DataTypes.STRING, allowNull:false},
+    rate: {type: DataTypes.REAL, allowNull: false, defaultValue: 5},
+    description: {type: DataTypes.TEXT, allowNull: false},
+    duration: {type: DataTypes.INTEGER, allowNull: false, defaultValue: 120},
+    startDate: {type: DataTypes.DATEONLY, allowNull: false},
+    genres: {type: DataTypes.ARRAY(DataTypes.TEXT), allowNull: false},
+    img: {type: DataTypes.STRING, allowNull: false}
+})
+
+const Actor = sequelize.define('actor', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING,  allowNull: false},
+    gender: {type: DataTypes.ENUM('М', 'Ж'), allowNull: false},
+    birthDate: {type: DataTypes.DATEONLY, allowNull: false},
+    country: {type: DataTypes.STRING, allowNull:false},
+    img: {type: DataTypes.STRING, allowNull: false} 
+})
+
+const Director = sequelize.define('director', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING,  allowNull: false},
+    gender: {type: DataTypes.ENUM('М', 'Ж'), allowNull: false},
+    birthDate: {type: DataTypes.DATEONLY, allowNull: false},
+    country: {type: DataTypes.STRING, allowNull:false},
+    img: {type: DataTypes.STRING, allowNull: false} 
+})
+
+const Seance = sequelize.define('seance', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    price: {type: DataTypes.INTEGER, allowNull: false, defaultValue: 200},
+    date: {type: DataTypes.DATE, allowNull: false}
+})
+
+const Cinema = sequelize.define('cinema', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    title: {type: DataTypes.STRING, allowNull: false},
+    adress: {type: DataTypes.STRING, allowNull: false},
+    phone: {type: DataTypes.STRING, allowNull: false},
+    img: {type: DataTypes.STRING, allowNull: false}
+})
+
+const Hall = sequelize.define('hall', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    number: {type: DataTypes.INTEGER, allowNull: false},
+    rows: {type: DataTypes.INTEGER, allowNull: false, defaultValue: 10},
+    seats: {type: DataTypes.INTEGER, allowNull: false, defaultValue: 12}
+})
+
+const SelectedSeat = sequelize.define('selectedSeat', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    seats: {type: DataTypes.ARRAY(DataTypes.INTEGER), allowNull: false}
+})
+
+const Client = sequelize.define('client', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING,  allowNull: false},
+    phone: {type: DataTypes.STRING, allowNull: false},
+    email: {type: DataTypes.STRING, allowNull: false}
+})
+
+const Order = sequelize.define('orders', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    date: {type: DataTypes.DATE, allowNull: false},
+    price: {type: DataTypes.INTEGER, allowNull: false}
+})
+
+const MovieActor = sequelize.define('movie_actor', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const MovieDirector = sequelize.define('movie_director', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+
+
+Movie.hasMany(Seance)
+Seance.belongsTo(Movie)
+
+Hall.hasMany(Seance)
+Seance.belongsTo(Hall)
+
+Cinema.hasMany(Seance)
+Seance.belongsTo(Cinema)
+
+Seance.hasMany(SelectedSeat)
+SelectedSeat.belongsTo(Seance)
+
+Client.hasMany(SelectedSeat)
+SelectedSeat.belongsTo(Client)
+
+Seance.hasMany(Order)
+Order.belongsTo(Seance)
+
+SelectedSeat.hasOne(Order)
+Order.belongsTo(SelectedSeat)
+
+Client.hasMany(Order)
+Order.belongsTo(Client)
+
+Movie.belongsToMany(Actor, {through: MovieActor})
+Actor.belongsToMany(Movie, {through: MovieActor})
+
+Movie.belongsToMany(Director, {through: MovieDirector})
+Director.belongsToMany(Movie, {through: MovieDirector})
+
+module.exports = {
+    Movie,
+    Actor,
+    Director,
+    Seance,
+    Cinema,
+    Hall,
+    Client,
+    Order,
+    SelectedSeat,
+    MovieActor,
+    MovieDirector
+}
+
+
